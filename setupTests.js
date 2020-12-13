b5cb4628dec861ec315db610c4415e4427da45f4
Teemu Orava <teemu.orava@kapsi.fi>
2020-12-13 18:43:45 +0200

    Updates dependencies. Updates to React 17.

--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,5 @@
 const enzyme = require('enzyme');
-const adapter = require('enzyme-adapter-react-16');
+const adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
 // Setup enzyme's react adapter
 enzyme.configure({
