0ad5a325f8cdc50e1f2f8e240b469c347c17aae9
Teemu Orava <teemu.orava@kapsi.fi>
2021-01-14 02:02:13 +0200

    Build with tsc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "jest": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      'jsx': true
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "react-hooks"
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "rules": {
-        "no-console": "off",
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn"
-    }
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
